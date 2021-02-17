9bce01549b52231c172b35b547d2e4770ff7a90e
huseyn222 <77275721+huseyn222@users.noreply.github.com>
2021-02-17 15:11:10 +0400

    Add files via upload

---
+++ Final00/javascript/app.js
@@ -0,0 +1,17 @@
+
+var xxmini=document.querySelector(".a1")
+var xx1=document.querySelector("#xx1")
+var remove=document.querySelector(".l71")
+xxmini.onclick=function(){
+  xx1.classList.remove("active")
+}
+
+
+remove.onclick=function(){
+  xx1.classList.add("active")
+}
+
+
+
+
+
